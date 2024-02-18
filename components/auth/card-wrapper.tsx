c346a3812fde9f23ff1a3572d63809c3aa26536f
ProfNuru <nurudeen.karim2016@gmail.com>
2024-02-18 22:01:11 +0000

    next app with base authJS authentication

---
+++ components/auth/card-wrapper.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { Card, CardContent, CardFooter, CardHeader } from "../ui/card";
+import BackButton from "./back-button";
+import Header from "./header";
+import Social from "./social";
+
+interface CardWrapperProps {
+  children: React.ReactNode;
+  headerLabel: string;
+  backButtonLabel: string;
+  backbuttonHref: string;
+  showSocial?: boolean;
+}
+
+const CardWrapper = ({
+  children,
+  headerLabel,
+  backButtonLabel,
+  backbuttonHref,
+  showSocial,
+}: CardWrapperProps) => {
+  return (
+    <Card className="w-[400px] shadow-md">
+      <CardHeader>
+        <Header label={headerLabel} />
+      </CardHeader>
+      <CardContent>{children}</CardContent>
+      {showSocial && (
+        <CardFooter>
+          <Social />
+        </CardFooter>
+      )}
+      <CardFooter>
+        <BackButton href={backbuttonHref} label={backButtonLabel} />
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default CardWrapper;
